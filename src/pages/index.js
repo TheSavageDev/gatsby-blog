3b2058b8d146c1c8b78d4723519bb19644e7ed64
Jason Savage <jason.savage@c2fo.com>
2021-08-16 10:46:19 -0500

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from 'react'
-import { Link } from 'gatsby';
-import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image'
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I am making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Three girls sitting on a concrete elephant statue, none of them looking at the camera."
+        src="../images/aaa.jpg"
+      />
     </Layout>
   )
 }
